ced474b241f75799e327b184e66d9e27ddd6f9f6
Na Hyung Choi and saswati patra <saswati@>
2019-06-10 10:46:19 -0700

    refactor JS script to use .forEach loop

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    event.preventDefault();
+    var inputs = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    inputs.forEach(function(input) {
+      var userInput = $("input#" + input).val();
+      $("." + input).text(userInput);
+    });
 
     $("#story").show();
-
-    event.preventDefault();
   });
 });
